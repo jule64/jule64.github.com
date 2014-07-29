6db269f3aa75346ec99fa2c26bc52d81302af2bf
Julien Monnier <jule64@gmail.com>
2014-07-29 12:56:54 +0100

    added disqus thread amend intro text

--- blogposts/playingwithAngularJS/script.js
+++ blogposts/playingwithAngularJS/script.js
@@ -50,13 +50,12 @@ angular.module("my_module",['ngSanitize'])
     };
 
 
-    $scope.introText = '<br>Alright not much to see here except this rather simplistic UI.  The cool thing is this page is 100% generated '
+    $scope.introText = '<br>Alright this UI on the left doesnt seem much but the cool thing is it was generated entirely '
         +'using <a target="_blank" href="https://angularjs.org/">AngularJS</a>.  I used an existing example that you can find <a target="_blank" href="http://tutorialzine.com/2013/08/learn-angularjs-5-examples/">here</a>'
         +' and I added a few other stuff like the NgSanitise library to display the html content of this introduction text properly (even though the text is saved as a plain string in my script.js file.) '
-        +'<br>I am really excited by AngularJS.  My first steps with Angular were not easy (I am also new to JavaScript which didnt help) but I '
-        +'am now ready to move to playing with partial views, adding services such as <a target="_blank" href="https://www.firebase.com/">Firebase</a>'
-        +' and other JavaScript technologies like Grunt (a build tool)';
-
+        +'<br>AngularJS is quite an exciting technology and even though it took me some time to initially "get it" (I am also new to JavaScript which didnt help) I '
+        +'am now ready to start playing with partial views, custom directives, services such as <a target="_blank" href="https://www.firebase.com/">Firebase</a>'
+        +' and add other JavaScript technologies like Grunt to my workflow (Oh I forgot to mention I have also used Bower to manage the dependencies in this page)';
 }]);
 
 //my_module.controller("TestController",['$scope',function($scope){
