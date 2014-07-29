b93bce44bc00c0659f9417603bebd45942a98e64
jule64@gmail.com <jule64@gmail.com>
2014-07-29 03:05:21 +0100

    redesign

--- blogposts/playingwithAngularJS/script.js
+++ blogposts/playingwithAngularJS/script.js
@@ -1,56 +1,67 @@
-function OrderFormController($scope){
 
-	// Define the model properties. The view will loop
-	// through the services array and generate a li
-	// element for every one of its items.
+angular.module("my_module",['ngSanitize'])
 
-	$scope.services = [
-		{
-			name: 'Web Development',
-			price: 300,
-			active:true
-		},{
-			name: 'Design',
-			price: 400,
-			active:false
-		},{
-			name: 'Integration',
-			price: 250,
-			active:false
-		},{
-			name: 'Training',
-			price: 220,
-			active:false
-		}
-	];
+.controller("OrderFormController",['$scope','$sce',function($scope,$sce){
 
-	$scope.toggleActive = function(s){
-		s.active = !s.active;
-	};
+    // Define the model properties. The view will loop
+    // through the services array and generate a li
+    // element for every one of its items.
 
-	// Helper method for calculating the total price
+    $scope.services = [
+        {
+            name: 'Web Development',
+            price: 300,
+            active:true
+        },{
+            name: 'Design',
+            price: 400,
+            active:false
+        },{
+            name: 'Integration',
+            price: 250,
+            active:false
+        },{
+            name: 'Training',
+            price: 220,
+            active:false
+        }
+    ];
 
-	$scope.total = function(){
+    $scope.toggleActive = function(s){
+        s.active = !s.active;
+    };
 
-		var total = 0;
+    // Helper method for calculating the total price
 
-		// Use the angular forEach helper method to
-		// loop through the services array:
+    $scope.total = function(){
 
-		angular.forEach($scope.services, function(s){
-			if (s.active){
-				total+= s.price;
-			}
-		});
+        var total = 0;
 
-		return total;
-	};
-}
+        // Use the angular forEach helper method to
+        // loop through the services array:
+
+        angular.forEach($scope.services, function(s){
+            if (s.active){
+                total+= s.price;
+            }
+        });
+
+        return total;
+    };
 
 
-function TestController($scope){
-	
-	$scope.introText = 'This is an example page built using AngularJS.  This example comes from <a target="_blank" href="http://tutorialzine.com/2013/08/learn-angularjs-5-examples/">this blog</a> (see example 3: Order Form)<br>If you look in the source of my page, towards the top you will find a tag called script that contains the link to the angular.min.js script. This is all you need to run an AngularJS-enabled page.<br>I am quite excited by this technology as it allows to build really clean, expressive and well organised web pages.  It also allows to create partials easily (it is a technique that lets you refresh portions of a page rather than refreshing the entire page).  Pretty cool.<br>I will continue to explore this technology with the aim of converting all of my blog to Angular and will also attempt to integrate other Angular friendly services such as <a target="_blank" href="https://www.firebase.com/">Firebase</a>';
-	
-}
+    $scope.introText = '<br>Alright not much to see here except this rather simplistic UI.  The cool thing is this page is 100% generated '
+        +'using <a target="_blank" href="https://angularjs.org/">AngularJS</a>.  I used an existing example that you can find <a target="_blank" href="http://tutorialzine.com/2013/08/learn-angularjs-5-examples/">here</a>'
+        +' and I added a few other stuff like the NgSanitise library to display the html content of this introduction text properly (even though the text is saved as a plain string in my script.js file.) '
+        +'<br>I am really excited by AngularJS.  My first steps with Angular were not easy (I am also new to JavaScript which didnt help) but I '
+        +'am now ready to move to playing with partial views, adding services such as <a target="_blank" href="https://www.firebase.com/">Firebase</a>'
+        +' and other JavaScript technologies like Grunt (a build tool)';
+
+}]);
+
+//my_module.controller("TestController",['$scope',function($scope){
+//
+//
+//
+//}]);
 
